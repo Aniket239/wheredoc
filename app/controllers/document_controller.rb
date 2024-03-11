5d63c32e2b5f69623386bbe70990ef04fa252dc4
Aniket Biswas <aniketbiswas2392001@gmail.com>
2024-03-11 19:10:04 +0530

    comit

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -4,7 +4,8 @@ class DocumentController < ApplicationController
   end
 
   def new
-    
+    @unique_departments = Employee.select(:employee_department).distinct.pluck(:employee_department)
+    @unique_employees = Employee.select(:employee_name).distinct.pluck(:employee_name)
   end
 
   def edit
