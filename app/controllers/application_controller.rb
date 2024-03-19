41de8e826ffcf50a171b6e52f13645d5582d4c19
Aniket Biswas <aniketbiswas2392001@gmail.com>
2024-03-19 18:36:12 +0530

    login logout system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,40 @@
-# app/controllers/application_controller.rb
-
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?
-  
-    private
-  
-    def log_in(employee)
-      p "======================== session ========================================"
-      p session[:user_id] = employee.id
-      p "============================= session ==================================="
-    end
-  
-    def log_out
-      p "======================== session logout ================================="
-      p session.delete(:user_id)
-      p "======================== session logout ================================="
-      @current_user = nil
-    end
-  
-    def current_user
-      p "======================== current user ================================="
-      @current_user_email ||= Employee.where(id: session[:user_id]).select(:employee_email).first&.employee_email if session[:user_id]
-      p @current_user_email
-      p "======================== current user ================================="
-    end
-  
-    def logged_in?
-      !current_user.nil?
+  helper_method :current_user, :logged_in?
+
+  before_action :authenticate_user!
+
+  private
+
+  def log_in(employee)
+    p "======================== cookies ========================================"
+    cookies[:user_id] = employee.id
+    p cookies[:user_id]
+    p "============================= cookies ==================================="
+  end
+
+  def log_out
+    p "======================== cookies logout ================================="
+    cookies.delete(:user_id)
+    p "======================== cookies logout ================================="
+    @current_user = nil
+  end
+
+  def current_user
+    p "======================== current user ================================="
+    @current_user_email ||= Employee.find_by(id: cookies[:user_id])&.employee_email if cookies[:user_id].present?
+    p @current_user_email
+    p "======================== current user ================================="
+    @current_user_email
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authenticate_user!
+    unless logged_in?
+      p "======================== redirect to login ============================="
+      redirect_to setup_login_path
     end
   end
-  
+end
--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -1,4 +1,6 @@
 class SetupController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:login, :signup, :create, :update]
+
   def index
   end
   def employees
